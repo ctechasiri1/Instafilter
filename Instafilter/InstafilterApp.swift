a1d07e6a9ba1f83a7eca9ad34aa852eb05388580
Chiraphat Techasiri <ctechasiri@gmail.com>
2025-07-03 21:10:44 -0700

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Chiraphat Techasiri on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
